e2e1e3cc7c42bbd32383f03a51cdf424961f6b01
Luiz Silveira <luiz.dsilveira@stone.com.br>
2024-10-30 18:27:27 -0300

    add service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AnimalsModule } from './animals/animals.module';
 import { AnimalsController } from './animals/animals.controller';
+import { AnimalsService } from './animals/animals.service';
 
 @Module({
   imports: [AnimalsModule],
   controllers: [AppController, AnimalsController],
-  providers: [AppService],
+  providers: [AppService, AnimalsService],
 })
 export class AppModule {}
